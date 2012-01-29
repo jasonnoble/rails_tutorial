884de2d227d3121652376d8f245952b58fcdddf1
Jason Noble <perlwizard@gmail.com>
2012-01-29 15:52:24 -0700

    Remove microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,31 +4,63 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', :text => heading) }
+    it { should have_selector('title', :text => full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', :text => 'Sample App') }
-    it { should have_selector('title', :text => full_title('Home')) }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { 'Home' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => full_title("Help")) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
     
-    it { should have_selector('h1', :text => 'About Us') }
-    it { should have_selector('title', :text => full_title("About Us")) }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
   end
   
-  describe "Contact" do
+  describe "Contact page" do
     before { visit contact_path }
-    
-    it { should have_selector('h1', :text => 'Contact') }
-    it { should have_selector('title', :text => full_title("Contact")) }
+
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+
+    click_link 'About'
+    page.should have_selector 'title', text: full_title('About Us')
+
+    click_link 'Help'
+    page.should have_selector 'title', text: full_title('Help')
+
+    click_link 'Contact'
+    page.should have_selector 'title', text: full_title('Contact')
+
+    click_link 'Home'
+    page.should have_selector 'title', text: full_title('Home')
+
+    click_link 'Sign up now!'
+    page.should have_selector 'title', text: full_title('Sign up')
   end
 end
